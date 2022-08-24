608af0b982a29e3652879331c6e284aad150df7e
camomilaa <camilasoarsilvaa@gmail.com>
2022-08-24 15:42:12 -0300

    Aula 2

---
+++ Aula_2/Exercicios/Questao2Carro.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao2Carro {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        String[] caractere;
+
+        System.out.println("Digite a placa do seu carro: ");
+        String placa = leitor.nextLine().toLowerCase();
+
+        System.out.println("Digite o ano de fabricação do carro: ");
+        int ano = leitor.nextInt();
+
+        //mostrando estado do carro
+        if(ano<2010){
+            System.out.println("Carro velho");
+        } else if(ano<2021){
+            System.out.println("Carro semi novo");
+        } else{
+            System.out.println("Carro novo");
+        }
+
+        //ocultando vogais da placa
+        caractere = placa.split("");
+        placa = "";
+        for(int i = 0; i<caractere.length; i++){
+            if(caractere[i].equals("a")||caractere[i].equals("e")||caractere[i].equals("i")||caractere[i].equals("o")||caractere[i].equals("u")){
+                caractere[i] = "*";
+            }
+            placa += caractere[i];
+        }
+        System.out.println(placa.toUpperCase());
+    }
+}
---
+++ Aula_2/Exercicios/Questao2Frase.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Questao2Frase {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        String[] fraseDesconstruida;
+        int vogais = 0;
+        var hashset = new HashSet<String>();
+
+        System.out.println("Digite a frase: ");
+        String frase = leitor.nextLine();
+        fraseDesconstruida = frase.toLowerCase().split("");
+
+        for(int i=0; i< fraseDesconstruida.length; i++){
+            if(fraseDesconstruida[i].equals("a")||fraseDesconstruida[i].equals("e")||fraseDesconstruida[i].equals("i")||fraseDesconstruida[i].equals("o")||fraseDesconstruida[i].equals("u")){
+                hashset.add(fraseDesconstruida[i]);
+            }
+        }
+        vogais = hashset.size();
+
+        System.out.println("Existem " + vogais + " nessa frase");
+
+    }
+}
